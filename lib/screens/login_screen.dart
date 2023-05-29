618a6fa1aebad1be4a64aaa849f8a7e9e1c417a8
Mario Jose Ramos Mejia <marioramosmejia2243@gmail.com>
2023-05-29 00:00:43 -0600

    Agregando Validacion de los inputs del formulario

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/provider/login_form_provider.dart';
 import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -25,11 +27,15 @@ class LoginScreen extends StatelessWidget {
                     style: TextStyle(
                       color: Color.fromARGB(255, 2, 11, 131),
                       fontSize: 50,
-                      fontWeight: FontWeight.bold
+                      fontWeight: FontWeight.bold,
                     ) 
                 ),
                 const SizedBox(height: 30),
-                _LoginForm(),
+                ChangeNotifierProvider(
+                  create: (_)=> LoginFormProvider(),
+                  child: _LoginForm(),
+                ),
+                
               ],
             ),
           ),
@@ -50,33 +56,61 @@ class _LoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final loginForm = Provider.of<LoginFormProvider>(context);
+    
     return Form(
+      key: loginForm.formKey,
+      autovalidateMode:AutovalidateMode.always,
       child: Column(
         children: [
           TextFormField(
             autocorrect: false,
             keyboardType: TextInputType.emailAddress,
             decoration: InputDecorations.authInputDecoration(hintText:'Mario@gmail.com',label: 'Correo',icono: Icons.alternate_email),
+            validator: (value){
+              String pattern = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+
+              RegExp regExp  =  RegExp(pattern);
+
+              return regExp.hasMatch(value ?? '' ) ? null : 'Correo Incorrecto';
+            },
+            onChanged: (value) => loginForm.email =value,
+            
           ),
           TextFormField(
             autocorrect: false,
             obscureText: true,
             decoration: InputDecorations.authInputDecoration(hintText:'******',label: 'Contraseña',icono: Icons.lock_outline),
+            validator: (value){
+              if(value != null && value.length>=6) return null;
+              return 'La contraseña debe de ser de 6 caracteres';
+            },
+            onChanged: (value) => loginForm.password =value,
           ),
           const SizedBox(height: 30,),
           MaterialButton(
-            onPressed: (){
-            
+            onPressed: loginForm.isLoading ? null :  () async{
+              FocusScope.of(context).unfocus();
+              if(!loginForm.isValidForm())return;
+              loginForm.isLoading=true;
+
+              await Future.delayed( const Duration(seconds: 2));
+
+              loginForm.isLoading=false;
+              
+              Navigator.pushReplacementNamed(context, 'home');
             },
             color: Colors.deepPurple,
             disabledColor: Colors.grey,
+            
             shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
             elevation: 0,
             child: Container(
               padding: const EdgeInsets.all(15),
-              child: const Text(
-                'Ingresar',
-                style: TextStyle(color: Colors.white)
+              child:  Text(
+                loginForm.isLoading ? 'Espere' : 'Ingresar',
+                style: const TextStyle(color: Colors.white)
               )
             ),
           )
